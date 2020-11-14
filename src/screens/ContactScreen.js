6b34ade2bfdd2ece93172eeedc59d2e81625a04a
ismail T <smileyteke@hotmail.com>
2020-11-14 12:10:09 +1100

    update

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -5,7 +5,7 @@ import React from 'react';
 function ContactScreen (props) {    
     return    <div className='email'>               
                <h4> eledaalter@hotmail.com</h4> 
-               <a href="https://www.facebook.com/eledaalterations" target='blank' class="fa fa-facebook"></a>
+               <a href="https://www.facebook.com/eledaalterations" target='blank' class="fa fa-facebook">acebook</a>
                </div>                            
            
               
